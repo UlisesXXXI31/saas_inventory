84f40e6c40b93efcdcaf7de2a0cce44ed66fdea3
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2026-05-07 13:14:04 +0000

    CommandLineRunner to drop the data in the tables

--- saas-api/saas-api/src/main/java/com/saas/saas_api/SaasApiApplication.java
+++ saas-api/saas-api/src/main/java/com/saas/saas_api/SaasApiApplication.java
@@ -9,5 +9,18 @@ public class SaasApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaasApiApplication.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner initDatabase() {
+        return args -> {
+            System.out.println("--- INICIANDO LIMPIEZA DE BASE DE DATOS ---");
+            try {
+                // Ejecutamos el comando SQL que resetea todo
+                jdbcTemplate.execute("TRUNCATE TABLE productos, empresas RESTART IDENTITY CASCADE");
+                System.out.println("✅ ÉXITO: Tablas vaciadas e IDs reiniciados a 1");
+            } catch (Exception e) {
+                System.out.println("❌ ERROR: " + e.getMessage());
+            }
+        };
 
 }
